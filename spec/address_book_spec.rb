7e65dd8ad5fe2510cac056143406833e71723122
Brandon Baker <brandonbaker40@icloud.com>
2017-01-24 19:33:46 -0500

    first dabble in tdd

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -18,6 +18,23 @@ require_relative '../models/address_book'
       end
     end
 
+    describe "#remove_entry" do
+       it "removes an entry from the address book" do
+          book = AddressBook.new
+          book.add_entry("Michael Scott", "301.555.5555", "michael@dundermifflin.com")
+
+          name = "Ada Lovelace"
+          phone_number = "010.012.1815"
+          email = "augusta.king@lovelace.com"
+          book.add_entry(name, phone_number, email)
+
+          expect(book.entries.size).to eq 2
+          book.remove_entry(name, phone_number, email)
+          expect(book.entries.size).to eq 1
+          expect(book.entries.first.name).to eq("Michael Scott")
+       end
+    end
+
    describe "#add_entry" do
      it "adds only one entry to the address book" do
        book = AddressBook.new
@@ -36,4 +53,5 @@ require_relative '../models/address_book'
        expect(new_entry.email).to eq('augusta.king@lovelace.com')
      end
    end
+
  end
